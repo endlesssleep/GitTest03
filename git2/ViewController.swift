8cc47fd54bf9be4b41e21cf388f0fad9eb9284eb
akzmffls <d7703_14@d7703-14ui-imac.local>
2019-03-07 11:43:17 +0900

    Initial Commit

---
+++ git2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  git2
+//
+//  Created by D7703_14 on 2019. 3. 7..
+//  Copyright © 2019년 Personal Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
